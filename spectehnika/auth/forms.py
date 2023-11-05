ca856517acf9f145f63afb2579a03bbc2a886912
Rustam <spalm@list.ru>
2023-11-05 21:38:58 +0300

    something in LoginForm

--- spectehnika/auth/forms.py
+++ spectehnika/auth/forms.py
@@ -1,5 +1,8 @@
-from wtforms import Form
+from wtforms import Form, EmailField, PasswordField, validators, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(Form):
-    ...
+    email = EmailField('Email', [validators.Email()])
+    password = PasswordField('Пароль',  validators=[DataRequired(), Length(min=4, max=100)])
+    # submit = SubmitField("Войти")
