5ff038d6febab8e15053c6e9ceaaf0cdbf94ece5
Rustam <spalm@list.ru>
2023-11-05 21:39:23 +0300

    add new models

--- spectehnika/auth/models.py
+++ spectehnika/auth/models.py
@@ -2,7 +2,7 @@ from db import BaseModel, db
 
 from flask_login import UserMixin
 
-from peewee import CharField, IntegerField, DateField, ForeignKeyField
+from peewee import CharField, IntegerField, DateField, ForeignKeyField, BooleanField
 
 
 class Role(BaseModel):
@@ -15,4 +15,20 @@ class User(BaseModel, UserMixin):
     password = CharField()
     creation_date = DateField()
     role = ForeignKeyField(Role)
+    is_admin = BooleanField()
 
+
+class MachineTypes(BaseModel):
+    title = CharField()
+
+
+class Machine(BaseModel):
+    model = CharField()
+    type = ForeignKeyField(MachineTypes)
+    number = CharField(unique=True)
+
+
+models = [Role, User]
+
+# db.create_tables(models)
+# db.drop_tables(models)
