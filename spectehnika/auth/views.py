408d0b4414fd4d5c6f863a5f198814abac5441a2
Rustam <spalm@list.ru>
2023-11-05 21:40:11 +0300

    commenting POST login dont work :(

--- spectehnika/auth/views.py
+++ spectehnika/auth/views.py
@@ -1,8 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Flask, render_template, redirect, url_for, request, make_response, Blueprint
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+from auth.forms import LoginForm
+from auth.models import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @bp.get('/login')
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    context = {
+        'form': form
+    }
+    return render_template('auth/login.html', **context)
+
+
+# @bp.post('/login')
+# def login():
+#     form = LoginForm(request.form)
+#     user = User.get_or_none((User.email == form['email']) & (User.password == form['password']))
+#     if user is None:
+#         return redirect(url_for('login'))
+#     else:
+#         return redirect('auth/home_page.html')
+
+
+@bp.get('/user_home_page')
+def home_page():
+    return render_template('user/user_home_page.html')
