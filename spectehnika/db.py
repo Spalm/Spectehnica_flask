6033a7dcb5b0b1444e25a4304f1905c1d2d6f01d
Rustam <spalm@list.ru>
2023-11-21 22:24:38 +0300

    add user password in dp.py

--- spectehnika/db.py
+++ spectehnika/db.py
@@ -1,12 +1,16 @@
 import os
 
+from dotenv import load_dotenv
 from peewee import PostgresqlDatabase, Model
+
+load_dotenv()
 
 db = PostgresqlDatabase(
     os.environ.get('DB_NAME'),
     host=os.environ.get('DB_HOST'),
     port=os.environ.get('DB_PORT'),
-    user=os.environ.get('DB_USER')
+    user=os.environ.get('DB_USER'),
+    password=os.environ.get('DB_PASSWORD')
 )
 
 
