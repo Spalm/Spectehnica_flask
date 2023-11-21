1f09a1e9b5f62869ca0aeb9114e525eb62f85125
Rustam <spalm@list.ru>
2023-11-21 22:26:27 +0300

    create core/forms.py

---
+++ spectehnika/core/forms.py
@@ -0,0 +1,28 @@
+from wtforms import Form, DateField, BooleanField, StringField, IntegerField, SelectField
+
+from auth.models import Machine, Owner
+
+
+class ReportForm(Form):
+    date1 = DateField('начальная дата')
+    date2 = DateField('конечная дата')
+    owner = SelectField('поставщик')
+    machine = SelectField('модель')
+
+
+class DataForm(Form):
+    date = DateField('дата')
+    address = StringField('адрес')
+    owner = SelectField('поставщик', choices=['поставщик1', 'поставщик2', 'поставщик3'])
+    model = SelectField('модель',  choices=['экскаватор-погрузчик', 'мини-погрузчик', 'мини-экскаватор', 'колесный экскаватор'])
+    hours = IntegerField('часы')
+    cost = IntegerField('закупка')
+    sell = IntegerField('продажа')
+
+
+class AddSubleaseMachine(Form):
+    model = StringField('модель')
+    owner = SelectField('поставщик', choices=['поставщик1', 'поставщик2', 'поставщик3'])
+    type = SelectField('тип', choices=['экскаватор-погрузчик', 'мини-погрузчик', 'мини-экскаватор', 'колесный экскаватор'])
+
+
