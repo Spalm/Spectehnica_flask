a5d171e5004f30073a80c99a757186173ebc7810
Rustam <spalm@list.ru>
2023-11-21 22:22:40 +0300

    add login_manager in app.py

--- spectehnika/app.py
+++ spectehnika/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 
-from spectehnika.auth.views import bp as auth_bp
+from auth.manager import login_manager
+from auth.views import bp as auth_bp
+from core.views import bp as core_bp
+from admin.views import bp as admin_bp
 
 app = Flask(__name__)
+app.secret_key = 'b52f2d8e8500810394e55ec5b6eae445d96164786b516bb2010315a6b07962febce1db8accb0e9965258844a9d846d74'
+login_manager.init_app(app)
 app.register_blueprint(auth_bp)
+app.register_blueprint(core_bp)
+app.register_blueprint(admin_bp)
 
 if __name__ == '__main__':
     app.run()
