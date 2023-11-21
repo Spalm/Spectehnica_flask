4d5e903403a4a54dc92fb7f902cf0faf3869c37e
Rustam <spalm@list.ru>
2023-11-21 22:25:59 +0300

    add validator in LoginForm() auth/forms

--- spectehnika/auth/forms.py
+++ spectehnika/auth/forms.py
@@ -1,8 +1,27 @@
-from wtforms import Form, EmailField, PasswordField, validators, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import Form, EmailField, PasswordField, validators
+from wtforms.validators import InputRequired, Length
+
+from auth.models import User
 
 
 class LoginForm(Form):
-    email = EmailField('Email', [validators.Email()])
-    password = PasswordField('Пароль',  validators=[DataRequired(), Length(min=4, max=100)])
-    # submit = SubmitField("Войти")
+    email = EmailField('Email', validators=[
+        InputRequired('Введите email')
+    ]
+                       )
+    password = PasswordField('Пароль', validators=[
+        InputRequired('Введите пароль'),
+        Length(min=4, max=100)
+    ]
+                             )
+
+    def validate(self, extra_validators=None) -> bool:
+        if not super().validate(extra_validators):
+            return False
+
+        user = User.get_or_none(email=self.email.data, password=self.password.data)
+        if user is None:
+            self.password.errors.append('Неверный email или пароль')
+            return False
+
+        return True
