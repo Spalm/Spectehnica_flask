b6b515d41c1d1917c7476a47b5243d61867f498d
Rustam <spalm@list.ru>
2023-11-21 22:30:12 +0300

    create htmx <select> in main.py

---
+++ spectehnika/auth/manager.py
@@ -0,0 +1,12 @@
+from flask_login import LoginManager
+
+from auth.models import User
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+
+@login_manager.user_loader
+def load_user(user_id: str) -> int:
+    return User.get_by_id(int(user_id))
--- spectehnika/auth/views.py
+++ spectehnika/auth/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, make_response, Blueprint
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, login_required, current_user
 
 from auth.forms import LoginForm
 from auth.models import User
@@ -10,22 +10,22 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.get('/login')
 def login():
     form = LoginForm()
-    context = {
-        'form': form
-    }
-    return render_template('auth/login.html', **context)
+    return render_template('auth/login.html', form=form)
 
 
-# @bp.post('/login')
-# def login():
-#     form = LoginForm(request.form)
-#     user = User.get_or_none((User.email == form['email']) & (User.password == form['password']))
-#     if user is None:
-#         return redirect(url_for('login'))
-#     else:
-#         return redirect('auth/home_page.html')
+@bp.post('/login')
+def login_post():
+    form = LoginForm(request.form)
+    if not form.validate():
+        return render_template('auth/login.html', form=form)
+
+    user = User.get(email=form.email.data)
+    login_user(user)
+    return redirect(url_for('core.main'))
 
 
-@bp.get('/user_home_page')
-def home_page():
-    return render_template('user/user_home_page.html')
+@bp.get('/logout')
+@login_required
+def logout():
+    form = LoginForm(request.form)
+    return render_template('auth/login.html', form=form)
