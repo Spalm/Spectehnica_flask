13345c3bac039dd262bfad2556d0a41dd7bd9da1
Rustam <spalm@list.ru>
2023-11-23 18:07:18 +0300

    del SubLease

--- spectehnika/core/views.py
+++ spectehnika/core/views.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 
 from auth.forms import LoginForm
 from auth.models import User, Owner
-from core.forms import ReportForm, DataForm, AddSubleaseMachine
+from core.forms import ReportForm, DataForm
 
 from auth.models import Machine
 
