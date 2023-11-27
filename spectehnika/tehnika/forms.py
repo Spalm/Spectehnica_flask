f399d815c7dad87b058adf5284f2f12fa787b1bc
Rustam <spalm@list.ru>
2023-11-27 15:04:07 +0300

    create form AddType

--- spectehnika/tehnika/forms.py
+++ spectehnika/tehnika/forms.py
@@ -1,12 +1,28 @@
 from wtforms import Form, DateField, BooleanField, StringField, IntegerField, SelectField, EmailField, PasswordField
 from wtforms.validators import InputRequired, Length
 
+from auth.models import MachineTypes
+
 
 class AddTehniks(Form):
     model = StringField('модель')
     type = SelectField('тип')
     number = StringField('гос номер')
-    owner = StringField('поставщик')
+    owner = SelectField('поставщик')
     user = SelectField('машинист')
 
+
+class AddType(Form):
+    title = StringField('тип техники')
+
+    def validate(self, extra_validators=None) -> bool:
+        if not super().validate(extra_validators):
+            return False
+
+        type_ = MachineTypes.get_or_none(title=self.title.data)
+        if type_ is None:
+            return True
+        else:
+            self.title.errors.append('Такой поставщик уже есть')
+            return False
 
--- spectehnika/tehnika/views.py
+++ spectehnika/tehnika/views.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, redirect, url_for, request, make_respo
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 from auth.models import Machine, User, MachineTypes
-from tehnika.forms import AddTehniks
+from tehnika.forms import AddTehniks, AddType
 
 bp = Blueprint('teh', __name__, url_prefix='/teh')
 
 
 @bp.get('/tehnika')
 def tehnika():
+    type_form = AddType()
     form = AddTehniks()
     tehniks = Machine.select(Machine.model, Machine.user, Machine.number).join(User).where(Machine.owner == 1)
 
@@ -22,13 +23,15 @@ def tehnika():
 
     context = {
         'tehniks': tehniks,
-        'form': form
+        'form': form,
+        'type_form': type_form
     }
     return render_template('teh/tehnika.html', **context)
 
 
-@bp.post('/addtehnika')
+@bp.post('/add_tehnika')
 def add_tehnika():
+    type_form = AddType()
     form = AddTehniks(request.form)
     model = form['model'].data
     number = form['number'].data
@@ -48,8 +51,38 @@ def add_tehnika():
 
     context = {
         'tehniks': tehniks,
-        'form': form
+        'form': form,
+        'type_form': type_form
     }
     return render_template('teh/tehnika.html', **context)
 
 
+@bp.post('/add_type')
+def add_type():
+    form_type = AddType(request.form)
+    title = form_type['title'].data
+    # new_type = MachineTypes()
+    # form.populate_obj(new_type)
+    # new_type.save()
+    """Не срабатывает валидация на форме"""
+    MachineTypes.create(title=title)
+
+    type_form = AddType()
+    form = AddTehniks()
+    tehniks = Machine.select(Machine.model, Machine.user, Machine.number).join(User).where(Machine.owner == 1)
+
+    users = User.select(User.id, User.name)
+    users_choise = [(user.id, user.name) for user in users]
+    form.user.choices = users_choise
+
+    types = MachineTypes.select(MachineTypes.id, MachineTypes.title)
+    types_choises = [(type_.id, type_.title) for type_ in types]
+    form.type.choices = types_choises
+
+    context = {
+        'tehniks': tehniks,
+        'form': form,
+        'type_form': type_form
+    }
+
+    return render_template('teh/tehnika.html', **context)
