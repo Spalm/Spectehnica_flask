e0a4383474b508390b790a0976828a1cf5bfb95e
Rustam <spalm@list.ru>
2023-11-28 22:25:40 +0300

    create get_roles() for role = SelectField

--- spectehnika/emp/forms.py
+++ spectehnika/emp/forms.py
@@ -1,12 +1,20 @@
+from functools import cache
+
 from wtforms import Form, DateField, BooleanField, StringField, IntegerField, SelectField, EmailField, PasswordField
 from wtforms.validators import InputRequired, Length
 
 from auth.models import User, Role
 
 
+@cache
+def get_roles():
+    roles = Role.select(Role.id, Role.title)
+    return [(role.id, role.title) for role in roles]
+
+
 class Employee(Form):
     name = StringField('имя')
-    role = SelectField('должность')
+    role = SelectField('должность', choices=get_roles())
     email = EmailField('почта', validators=[InputRequired('Введите email')])
     password = PasswordField('пароль', validators=[InputRequired('Введите пароль'),
                                                    Length(min=4, max=100)])
@@ -16,11 +24,9 @@ class Employee(Form):
             return False
 
         user = User.get_or_none(email=self.email.data)
-        if user is None:
-            self.password.errors.append('Пользователь добавлен')
-            return True
-        else:
-            self.password.errors.append('Пользователь с такой почтой уже ест')
+        if user is not None:
+            self.password.errors.append('Пользователь с такой почтой уже есть')
             return False
+        return True
 
 
