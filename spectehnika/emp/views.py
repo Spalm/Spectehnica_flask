30b57b0536e31e6c6af09963e82a410e426b7253
Rustam <spalm@list.ru>
2023-11-28 22:20:31 +0300

    fix form  to add ib db & redirect

--- spectehnika/emp/views.py
+++ spectehnika/emp/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
 
-from flask import Flask, render_template, redirect, url_for, request, make_response, Blueprint
+from flask import Flask, render_template, redirect, url_for, request, make_response, Blueprint, flash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 from emp.forms import Employee
@@ -23,24 +23,20 @@ def employees():
     return render_template('emp/employees.html', **context)
 
 
-@bp.post('employees')
+@bp.post('/employees')
 def add_employees():
-    form = Employee(request.form)
-    name = form['name'].data
-    email = form['email'].data
-    password = form['password'].data
+    form_data = Employee(request.form)
+    if not form_data.validate():
+        return render_template('emp/employees.html', form=form_data)
+
+    name = form_data['name'].data
+    email = form_data['email'].data
+    password = form_data['password'].data
     creation_date = date.today()
-    role = form['role'].data
+    role = form_data['role'].data
     User.create(name=name, email=email, password=password, creation_date=creation_date,
                 role=role, is_admin=False)
 
-    roles = Role.select(Role.id, Role.title)
-    role_choises = [(role.id, role.title) for role in roles]
-    form.role.choices = role_choises
-    users = User.select(User.name, User.role).join(Role)
-    context = {
-        'users': users,
-        'form': form
-    }
-    return render_template('emp/employees.html', **context)
+    flash('Пользователь успешно создан')
+    return redirect(url_for('emp.employees'))
 
