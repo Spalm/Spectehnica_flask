916335d5fda3247da1c636a4b190fe5fc8bd7f7f
Rustam <spalm@list.ru>
2024-01-09 21:29:55 +0300

    add GetReportForm in view

--- spectehnika/admin/views.py
+++ spectehnika/admin/views.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, url_for, request, make_response, Blueprint
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
-from auth.models import Machine, User, Owner, MachineTypes
-from core.forms import ReportForm, DataForm
+from auth.models import Machine, User, Owner, MachineTypes, Report
+from core.forms import ReportForm, ReportRowForm, GetReportForm
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
@@ -10,16 +10,9 @@ bp = Blueprint('admin', __name__, url_prefix='/admin')
 @bp.get('/administration')
 @login_required
 def administration():
-    report_form = ReportForm()
-    owners = Owner.select(Owner.id, Owner.title)
-    owners_choices = [(owner.id, owner.title) for owner in owners]
-    owners_choices.insert(0, (0, 'Все поставщики'))
-    report_form.owner.choices = owners_choices
-
-    data_form = DataForm()
+    report_form = GetReportForm()
     context = {
         'report_form': report_form,
-        'data_form': data_form,
         'current_user': current_user
     }
     return render_template('admin/administration.html', **context)
@@ -29,16 +22,61 @@ def administration():
 def owners():
     owner_id = int(request.args.get('owner'))
     machines = Machine.select(Machine.type.distinct(), Machine.owner).join(MachineTypes)
-    if owner_id:
+    if owner_id != 0:
         machines = machines.where(Machine.owner_id == owner_id)
-    return render_template('core/machines.html', machines=machines)
+    additional_choices = [
+        (0, 'Все')
+    ]
+    context = {
+        'machines': machines,
+        'additional_choices': additional_choices,
+    }
+    return render_template('core/machines.html', **context)
 
 
 @bp.get('/models')
 def models():
-    type_id, owner_id = map(int, request.args.get('machines').split('_'))
-    models = (Machine.select(Machine.id, Machine.model)
-              .where((Machine.type_id == type_id) & (Machine.owner == owner_id)))
-    return render_template('core/models.html', models=models)
+    type_id, owner_id = map(int, request.args.get('machine').split('_'))
+    models = Machine.select(Machine.id, Machine.model)
+    if type_id != 0:
+        models = models.where((Machine.type_id == type_id) & (Machine.owner == owner_id))
+
+    additional_choices = [
+        (0, 'Все')
+    ]
+    context = {
+        'models': models,
+        'additional_choices': additional_choices,
+    }
+    return render_template('core/models.html', **context)
 
 
+@bp.post('report')
+def get_report():
+    form = GetReportForm(request.form)
+    start_date = form.date1.data
+    end_date = form.date2.data
+    owner = int(form.owner.data)
+    model = int(form.model.data)
+    type = int(form.machine.data.split('_')[0])
+
+    filters = Report.date.between(start_date, end_date)
+    if owner:
+        filters &= Report.owner == owner
+    if type:
+        filters &= Report.type == type
+    if model:
+        filters &= Report.model == model
+
+    reports = (Report
+               .select()
+               .join(Owner)
+               .where(filters))
+
+    context = {
+        'reports': reports,
+        'start_date': start_date.strftime('%d.%m.%Y'),
+        'end_date': end_date.strftime('%d.%m.%Y')
+    }
+
+    return render_template('report/report.html', **context)
--- spectehnika/core/views.py
+++ spectehnika/core/views.py
@@ -1,11 +1,10 @@
+import datetime
+
 from flask import Flask, render_template, redirect, url_for, request, make_response, Blueprint, flash
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_login import login_required, current_user
 
-from auth.forms import LoginForm
-from auth.models import User, Owner, MachineTypes
-from core.forms import ReportForm, DataForm, OwnerForm
-
-from auth.models import Machine
+from auth.models import User, Owner, Machine, Report, MachineTypes
+from core.forms import ReportForm, OwnerForm, GetReportForm, AddSublease
 from tehnika.forms import AddTehniks
 
 bp = Blueprint('core', __name__, url_prefix='/core')
@@ -14,16 +13,16 @@ bp = Blueprint('core', __name__, url_prefix='/core')
 @bp.get('/main')
 @login_required
 def main():
-    form = AddTehniks()
+    get_report = GetReportForm()
+    form = AddSublease()
     form_owner = OwnerForm()
-    data_form = DataForm()
     report_form = ReportForm()
     tehniks = Machine.select(Machine.model, Machine.user, Machine.number).join(User).where(Machine.owner == 1)
 
     context = {
         'form_owner': form_owner,
+        'get_report': get_report,
         'report_form': report_form,
-        'data_form': data_form,
         'tehniks': tehniks,
         'form': form,
         'current_user': current_user
@@ -33,8 +32,12 @@ def main():
 
 @bp.get('/owners')
 def owners():
-    owner_id = int(request.args.get('owner'))
-    machines = Machine.select(Machine.id, Machine.model)
+    keys = [arg for arg in request.args if arg.endswith('-owner')]
+    if len(keys) != 1:
+        raise ValueError('Multiple or none "-owner" args')
+
+    owner_id = int(request.args[keys[0]])
+    machines = Machine.select(Machine.type.distinct(), Machine.owner).join(MachineTypes)
     if owner_id:
         machines = machines.where(Machine.owner_id == owner_id)
     return render_template('core/machines.html', machines=machines)
@@ -42,24 +45,20 @@ def owners():
 
 @bp.get('/models')
 def models():
-    type_id, owner_id = map(int, request.args.get('machines').split('_'))
+    keys = [arg for arg in request.args if arg.endswith('-machine')]
+    if len(keys) != 1:
+        raise ValueError('Multiple or none "-owner" args')
+    result = request.args[keys[0]]
+    type_id, owner_id = map(int, result.split('_'))
     models = (Machine.select(Machine.id, Machine.model)
               .where((Machine.type_id == type_id) & (Machine.owner == owner_id)))
     return render_template('core/models.html', models=models)
 
 
-@bp.get('/add_owner')
-def add_owner():
-    return render_template('core/add_owner.html')
-
-
-# @bp.post('/new_owner')
-# def new_owner():
-#     form = request.form
-#     title = form['add_owner']
-#     Owner.create(title=title)
-#     flash('Поставщик успешно создан')
-#     return redirect(url_for('core.add_owner'))
+# @bp.get('/add_owner')
+# def add_owner():
+#     form_owner = OwnerForm()
+#     return render_template('core/add_owner.html', form_owner=form_owner)
 
 
 @bp.post('/new_owner')
@@ -67,7 +66,63 @@ def new_owner():
     form = OwnerForm(request.form)
     title = form['owner'].data
     if not form.validate():
-        return redirect(url_for('core.add_owner', result=0))
+        form.owner.data = ''
+        return render_template('core/add_owner.html', form=form)
     else:
+        form.owner.data = ''
         Owner.create(title=title)
-        return redirect(url_for('core.add_owner', result=1))
+        return render_template('core/add_owner.html', show_success_message=1, form=form)
+
+
+# @bp.get('/add_sublease')
+# def add_sublease():
+#     form = AddTehniks()
+#     return render_template('core/sublease_form.html', form=form)
+#
+
+@bp.post('/new_sublease')
+def new_sublease():
+    form = AddSublease(request.form)
+    if not form.validate():
+        form.model.data = ''
+        form.number.data = ''
+        return render_template('core/sublease_form.html', form=form)
+
+    owner = int(form.owner.data)
+    model = form.model.data
+    number = form.number.data
+    type_ = int(form.type.data)
+
+    form.model.data = ''
+    form.number.data = ''
+    Machine.create(owner=owner, model=model, number=number, type=type_, user=None)
+    return render_template('core/sublease_form.html', show_success_message=1, form=form)
+
+
+@bp.post('create_report')
+def create_report():
+    form = ReportForm(request.form)
+    for row in form:
+        if any(field is None for field in row.data.values()):
+            continue
+        date = row.data["date1"]
+        end_date = row.data["date2"]
+
+        days = 0
+        day = row.data["date1"]
+        while day <= end_date:
+            day += datetime.timedelta(days=1)
+            days += 1
+
+        hours = int(row.data["hours"]) / days
+
+        while date <= end_date:
+            type_ = row.data["machine"].split('_')[0]
+            Report.create(date=date, address=row.data["address"],
+                          owner=row.data["owner"], type=type_,
+                          model=row.data["model"], hours=hours,
+                          cost=row.data["cost"], price=row.data["price"])
+            date += datetime.timedelta(days=1)
+    return redirect(url_for('core.main'))
+
+
